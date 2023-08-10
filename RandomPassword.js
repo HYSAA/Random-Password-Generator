b1cdfa0b127be6c27abbae2527cbbb5ddf7a3340
HYSA <aj.anunciado@gmail.com>
2023-08-10 19:22:36 +0800

    will continue

---
+++ RandomPassword.js
@@ -0,0 +1,15 @@
+let btn= document.getElementsByClassName("button-el")
+let random=document.getElementsByClassName("random-el")
+let random2=document.getElementsByClassName("random1-el")
+
+let password=['a']
+
+
+let firstRandom= () =>{
+
+
+}
+
+
+let secondRandom
+
