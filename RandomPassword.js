bd8591cffa399c8de983858cd9699d3f9538ec86
HYSA <aj.anunciado@gmail.com>
2023-08-12 15:22:09 +0800

    error

--- RandomPassword.js
+++ RandomPassword.js
@@ -1,39 +1,4 @@
-// let btn= document.getElementById("button-el")
-// let random=document.getElementById("random-el")
-// let random2=document.getElementById("random1-el")
-// let result;
-// let password=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
-// "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-
-
-// function fight(){
-//     let index1= Math.floor(Math.random()*26)
-//     let index2= Math.floor(Math.random()*26)
-//     let fighters1= password[index1]
-//     let fighters2= password[index2]
-//     return [fighters1,fighters2]
-//  }
-
-
-// // let clickButton= () =>{
-// //     let result=fight()
-// //     random=result;
-// //     random2=result
-// //     random.textContent=result[0]
-// //     random2.textContent=result[1]
-// //     console.log(result)
-// // }
-// btn.addEventListener('click',function(){
-//     let result=fight()
-//     random=result;
-//     random2=result
-    
-//     random.textContent=result[0]
-//     random2.textContent=result[1]
-//     console.log(result)
-
-// })
 let btn= document.getElementById("button-el")
 let random=document.getElementById("random-el")
 let random2=document.getElementById("random1-el")
@@ -41,8 +6,6 @@ let test=document.getElementById("test-el")
 let result;
 let password=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
 "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-
 
 function fight(){
     let index1= Math.floor(Math.random()*26)
@@ -58,25 +21,24 @@ console.log(data)
  }
 
 
-btn.addEventListener('click',function(){
-    let result=fight()
-  
-    data=result
-    // random.textContent=result[0]
-    // document.getElementById("random-el").textContent=result[0]
+// btn.addEventListener('click',function(){
+//     let result=fight()//gi call niya ang function na fight()
+//     let random = document.getElementById("random-el")
+//     for (let i = 0; i < result.length; i++) {
+//         let joinedResult=result.join("")
+//         //let text = fruits.join(); JOIN SYNTAX
+//           random.textContent=joinedResult[i]
+//     //   random.textContent = result[i]
 
-    // random2.textContent=result[0]
-    // document.getElementById("random1-el").textContent=result[1]
-    
-    // for(let i=0;i<data.length;i++)
-    // random.textContent=data[i]
-    // document.getElementById("test-el").textContent=result[10]
+// }
+// })
+btn.addEventListener('click', function() {
+    let result = fight();
     let random = document.getElementById("random-el");
-    // let joinedData=password.join("")
-    for (let i = 0; i < data.length; i++) {
-        //let text = fruits.join(); JOIN SYNTAX
-        // data[i]=password.join("")
-      random.textContent = data[i]
-    //   =password.join("");
-}
-})
+    let joinedResult = result.join("");
+    random.textContent = joinedResult;
+  });
+
+// let a=["a", "b", "c", "d"]
+// let text=a.join("")
+// console.log(text)
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+let a=["a", "b", "c", "d"]
+let text=a.join("")
+console.log(text)
